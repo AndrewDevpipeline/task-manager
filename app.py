60042f1bbfd12c636fae833a438b388d444a142e
Aswaglord <andrew@blonquist.com>
2022-07-14 21:16:33 -0600

    moved init_app

--- app.py
+++ app.py
@@ -25,7 +25,6 @@ ma = Marshmallow(app)
 
 
 def create_all():
-    db.init_app(app)
     with app.app_context():
         db.create_all()
 
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,6 @@
-from app import app, create_all
+from app import app, create_all, db
+
+db.init_app(app)
 
 if __name__ == "__main__":
     create_all()
